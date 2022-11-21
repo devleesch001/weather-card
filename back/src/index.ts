603b4dd479d418d0e9c28867f365df3a8f5ee716
Alexis DEVLEESCHAUWER <alexis.devleeschauwer@imerir.com>
2022-11-21 13:47:40 +0100

    [None] Back - add eslint and prettier

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,18 +1,18 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 import ApiIndex from './api/index';
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
-app.use('/api', ApiIndex)
+app.use('/api', ApiIndex);
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);

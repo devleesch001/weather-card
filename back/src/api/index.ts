14cd4a6305ace0d6525876692977cb60e3f4a5bc
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-11-21 13:18:17 +0100

    init backend

---
+++ back/src/api/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import UserRoute from './UserRoute';
+const router = Router();
+
+router.get('/', (request, response) => {
+    response.send({message: { api: "ok" }});
+});
+
+router.use('/user', UserRoute)
+
+export default router;

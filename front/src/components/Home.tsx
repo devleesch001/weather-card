e0fd5ad5c4694a02689e5a9993467e79d523a28d
laurent <laurent.crouzil@imerir.com>
2022-11-21 17:19:26 +0100

    update weather

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -7,9 +7,9 @@ import Login from './Login'; //bouton menu
 function Home() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            {/*<MenuAppBar></MenuAppBar>
-            <WeatherhGrid></WeatherhGrid>*/}
-            <Login></Login>
+            <MenuAppBar></MenuAppBar>
+            <WeatherhGrid></WeatherhGrid>
+            {/*<Login></Login>*/}
         </Box>
     );
 }

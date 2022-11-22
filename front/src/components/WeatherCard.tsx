9eee6c3792d3ad2895a7e71e3d2ed1d81eeeefbb
laurent <laurent.crouzil@imerir.com>
2022-11-22 11:29:36 +0100

    add weather informations

--- front/src/components/WeatherCard.tsx
+++ front/src/components/WeatherCard.tsx
@@ -8,20 +8,67 @@ import AirIcon from '@mui/icons-material/Air';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import CloudIcon from '@mui/icons-material/Cloud';
-import { getWeather, WeatherInterface } from '../api/weather';
+import { getWeather, WeatherDataInterface, WeatherInterface } from '../api/weather';
+import { Typography } from '@mui/material';
+
+// interface pour information utilise
+export interface WeatherInfoInterface {
+    weatherId: number;
+    temp: number;
+    tempMin: number;
+    tempMax: number;
+    humidity: number;
+    windSpeed: number;
+}
+
+function ActionOnWeatherInterface(weatherData: WeatherDataInterface): WeatherInfoInterface {
+    const weatherInfo: WeatherInfoInterface = {
+        weatherId: weatherData.weather[0].id,
+        temp: weatherData.main.temp - 273.15,
+        tempMin: weatherData.main.temp_min - 273.15,
+        tempMax: weatherData.main.temp_max - 273.15,
+        humidity: weatherData.main.humidity,
+        windSpeed: weatherData.wind.speed,
+    };
+
+    return weatherInfo;
+}
 
 function WeatherCard() {
-    const [weather, setWeather] = React.useState<WeatherInterface | null>(null); //Hoock
+    const [weather, setWeather] = React.useState<WeatherDataInterface | null>(null); //Hoock
 
-    getWeather('paris').then((r) => {
-        const data = r.data as WeatherInterface;
-        setWeather(data);
-    });
+    React.useEffect(() => {
+        getWeather('paris').then((r) => {
+            const data = r.data as WeatherDataInterface;
+
+            setWeather(data);
+        });
+    }, []);
 
     return (
         <Card>
             <CardContent>
-                <WbSunnyIcon></WbSunnyIcon>
+                {weather ? (
+                    weather.weather[0].id >= 200 && weather.weather[0].id <= 232 ? (
+                        <ThunderstormIcon></ThunderstormIcon> //orage
+                    ) : weather.weather[0].id >= 300 && weather.weather[0].id <= 321 ? (
+                        <ThunderstormIcon></ThunderstormIcon> //bruine modif icone
+                    ) : weather.weather[0].id >= 500 && weather.weather[0].id <= 531 ? (
+                        <ThunderstormIcon></ThunderstormIcon> //pluie legere
+                    ) : weather.weather[0].id >= 600 && weather.weather[0].id <= 622 ? (
+                        <AcUnitIcon></AcUnitIcon> // neige
+                    ) : weather.weather[0].id >= 701 && weather.weather[0].id <= 781 ? (
+                        <ThunderstormIcon></ThunderstormIcon> // brouillard
+                    ) : weather.weather[0].id == 800 ? (
+                        <WbSunnyIcon></WbSunnyIcon> //soleil
+                    ) : weather.weather[0].id >= 801 && weather.weather[0].id <= 804 ? (
+                        <CloudIcon></CloudIcon>
+                    ) : (
+                        <></>
+                    )
+                ) : (
+                    <>no data</>
+                )}
             </CardContent>
         </Card>
     );

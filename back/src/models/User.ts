5c478234ab358df138ff466680912892ad2c21c4
Alexis DEVLEESCHAUWER <alexis.devleeschauwer@imerir.com>
2022-11-22 15:11:12 +0100

    [#9, #5] API - add user route (create, find)

--- back/src/models/User.ts
+++ back/src/models/User.ts
@@ -1,17 +1,54 @@
-import { Schema, model } from 'mongoose';
+import { Document, model, Model, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import _ from 'lodash';
 
-export interface UserInterface {
+const SALT_ROUNDS = 10;
+
+export interface UserInformationInterface {
     username: string;
     email: string;
-    password: string;
 }
 
-export const UserSchema = new Schema<UserInterface>({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+export interface UserBaseInterface extends UserInformationInterface {
+    password?: string;
+}
+
+export interface UserInterface extends UserBaseInterface {
+    hashedPassword: string;
+}
+
+interface UserDocumentInterface extends UserInterface, Document {
+    toUserInformation: () => UserInformationInterface;
+    setPassword: (password: string) => Promise<void>;
+    checkPassword: (password: string) => Promise<boolean>;
+}
+
+interface UserModelInterface extends Model<UserDocumentInterface> {
+    findByEmailOrUsername: (email?: string | null, username?: string | null) => Promise<UserDocumentInterface>;
+}
+
+const UserSchema: Schema<UserDocumentInterface> = new Schema({
+    username: { type: String, required: true, index: { unique: true } },
+    email: { type: String, required: true, index: { unique: true } },
+    hashedPassword: { type: String, required: true },
 });
 
-const User = model<UserInterface>('User', UserSchema);
+UserSchema.methods.toUserInformation = function () {
+    return { username: this.username, email: this.email } as UserInformationInterface;
+};
+
+UserSchema.methods.setPassword = async function (password: string) {
+    this.hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+};
+
+UserSchema.methods.checkPassword = async function (password: string) {
+    return await bcrypt.compare(password, this.hashedPassword);
+};
+
+UserSchema.statics.findByEmailOrUsername = function (email: string | null = null, username: string | null = null) {
+    return this.findOne(_.omitBy({ email: email, username: username }, (v) => !v)).exec();
+};
+
+const User = model<UserDocumentInterface, UserModelInterface>('User', UserSchema);
 
 export default User;

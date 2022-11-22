4860bc877ab1dcc59fae3ad21d512ba707da9f49
Alexis DEVLEESCHAUWER <alexis.devleeschauwer@imerir.com>
2022-11-22 15:36:38 +0100

    add account icon

--- front/src/components/MenuAppBar.tsx
+++ front/src/components/MenuAppBar.tsx
@@ -5,9 +5,45 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { Box, Menu, MenuItem } from '@mui/material';
 
-function MenuAppBar() {
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+const MenuAppBar: React.FC = () => {
+    const menuId = 'primary-search-account-menu';
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
     return (
         <AppBar position="static">
             <Toolbar>
@@ -17,10 +53,23 @@ function MenuAppBar() {
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     News
                 </Typography>
-                <Button color="inherit">Login</Button>
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    <IconButton
+                        size="large"
+                        edge="end"
+                        aria-label="account of current user"
+                        aria-controls={menuId}
+                        aria-haspopup="true"
+                        onClick={handleProfileMenuOpen}
+                        color="inherit"
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                </Box>
             </Toolbar>
+            {renderMenu}
         </AppBar>
     );
-}
+};
 
 export default React.memo(MenuAppBar);

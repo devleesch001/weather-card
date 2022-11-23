049ed4574034901df361beb5d24bb76f9e128dff
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-11-23 19:31:43 +0100

    move route for resiter and change route /login and /register with prefix auth

---
+++ back/src/api/AuthenticationRoute.ts
@@ -0,0 +1,64 @@
+import { Router } from 'express';
+import User from '~/models/User';
+import { generateAccessToken } from '~/services/AuthenticationService';
+import { body, validationResult } from 'express-validator';
+
+const router = Router();
+
+router.post(
+    '/login',
+    body('email').isEmail().isLength({ min: 5, max: 128 }),
+    body('password').isLength({ min: 5, max: 128 }),
+    async (req, res) => {
+        const user = await User.findByEmailOrUsername(req.body.email);
+
+        if (!user) {
+            res.status(401).send({ message: 'invalid credentials' });
+            return;
+        }
+
+        const password = req.body['password'];
+
+        if (!(await user.checkPassword(password))) {
+            res.status(401).send({ message: 'invalid password' });
+            return;
+        }
+
+        const accessToken = generateAccessToken(user.toUserInformation());
+        res.send({
+            token: accessToken,
+        });
+    }
+);
+
+router.post(
+    '/register',
+    body('email').isEmail().isLength({ min: 5, max: 128 }),
+    body('username').isLength({ min: 5, max: 128 }),
+    body('password').isLength({ min: 5, max: 128 }),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const user = new User({
+            username: req.body.username,
+            email: req.body.email,
+        });
+
+        await user.setPassword(req.body.password);
+
+        user.save()
+            .then(() => {
+                const accessToken = generateAccessToken(user.toUserInformation());
+
+                res.status(201).send({ user: user.toUserInformation(), token: accessToken });
+            })
+            .catch(() => {
+                res.status(400);
+                res.send({ message: 'failed' });
+            });
+    }
+);
+export default router;
--- back/src/api/UserRoute.ts
+++ back/src/api/UserRoute.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import User from '~/models/User';
 import { auth } from '~/middleware/AuthenticateMiddleware';
-import { generateAccessToken } from '~/services/AuthenticationService';
 
 const router = Router();
 
@@ -29,28 +28,7 @@ router.post(
     body('username').isLength({ min: 5, max: 128 }),
     body('password').isLength({ min: 5, max: 128 }),
     async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const user = new User({
-            username: req.body.username,
-            email: req.body.email,
-        });
-
-        await user.setPassword(req.body.password);
-
-        user.save()
-            .then(() => {
-                const accessToken = generateAccessToken(user.toUserInformation());
-
-                res.status(201).send({ user: user.toUserInformation(), token: accessToken });
-            })
-            .catch(() => {
-                res.status(400);
-                res.send({ message: 'failed' });
-            });
+        res.redirect('/api/auth/register');
     }
 );
 

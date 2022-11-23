767f1f5d03f33ae6941bdd46cd475888d8bee36e
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-11-23 19:14:54 +0100

    fix redis and use ;env for mangodb and redis url

--- back/src/api/Weather/WeatherRoute.ts
+++ back/src/api/Weather/WeatherRoute.ts
@@ -1,5 +1,6 @@
 import { Request, Router } from 'express';
 import { geoCode, getWeather } from '~/services/openWeatherMapService';
+import RedisService from '~/services/RedisService';
 
 const router = Router();
 
@@ -16,42 +17,36 @@ router.get('/', async (request: Request, response) => {
     const query = request.query;
     const search = query['search'] as string;
 
-    response.send({
-        coord: { lon: 2.32, lat: 48.8589 },
-        weather: [{ id: 800, main: 'Clear', description: 'clear sky', icon: '01n' }],
-        base: 'stations',
-        main: { temp: 280.56, feels_like: 277.44, temp_min: 278.83, temp_max: 281.57, pressure: 994, humidity: 87 },
-        visibility: 10000,
-        wind: { speed: 5.14, deg: 250 },
-        clouds: { all: 0 },
-        dt: 1669059424,
-        sys: { type: 2, id: 2012208, country: 'FR', sunrise: 1669014525, sunset: 1669046681 },
-        timezone: 3600,
-        id: 6545270,
-        name: 'Palais-Royal',
-        cod: 200,
-    });
-    return;
-
     if (!search) {
         response.status(500).send({ error: 'bad request' });
         return;
     }
 
-    console.log(search);
+    const value = await RedisService.client.get(search);
 
-    const result = await geoCode(search);
-    const data = result.data as Geocoding[];
+    if (typeof value === 'string') {
+        response.send(JSON.parse(value));
+    } else {
+        const result = await geoCode(search);
+        const data = result.data as Geocoding[];
 
-    if (data.length === 0) {
-        response.status(500).send({ error: 'not found' });
-        return;
+        if (data.length === 0) {
+            response.status(500).send({ error: 'not found' });
+            return;
+        }
+
+        const weather = await getWeather(data[0].lat, data[0].lon);
+        const weatherData = weather.data;
+
+        RedisService.client
+            .set(search, JSON.stringify(weatherData), {
+                EX: 60,
+                NX: true,
+            })
+            .then();
+
+        response.send(weatherData);
     }
-
-    const weather = await getWeather(data[0].lat, data[0].lon);
-    console.log(weather.data);
-
-    response.send(weather.data);
 });
 
 export default router;
--- back/src/services/MangoDBService.ts
+++ back/src/services/MangoDBService.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const init = async () => {
-    await mongoose.connect('mongodb://root:example@127.0.0.1:27017');
+    await mongoose.connect(process.env.MANGODB_URL ?? 'mongodb://root:example@127.0.0.1:27017');
 };
 
 export default { init };

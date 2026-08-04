aea8001a5037597c3d8f0d972b217b78aaae2aba
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2026-08-04 16:03:00 +0200

    feat: enhance TelemetryRoute to conditionally initialize proxies for traces and metrics

--- back/src/api/TelemetryRoute.ts
+++ back/src/api/TelemetryRoute.ts
@@ -5,28 +5,34 @@ import process from "process";
 const router = Router();
 
 
-if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
-    console.info('Initialize OTEL traces Proxy');
-    router.use(
-        '/telemetry/traces',
-        createProxyMiddleware({
-            target: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-            changeOrigin: true,
-            pathRewrite: (path, req) => '/v1/traces',
-        })
-    );
+function isAllow(v?: string) {
+    return v === 'true' || v === '1' || v === 'on' || v === 'y'
 }
 
-if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
-    console.info('Initialize OTEL metrics Proxy');
-    router.use(
-        '/telemetry/metrics',
-        createProxyMiddleware({
-            target: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-            changeOrigin: true,
-            pathRewrite: (path, req) => '/v1/metrics',
-        })
-    );
+if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT && isAllow(process.env.OTEL_EXPORTER_ALLOW_PROXY)) {
+    if (process.env.OTEL_TRACES_EXPORTER === 'otlp') {
+        console.info('Initialize OTEL traces Proxy');
+        router.use(
+            '/telemetry/traces',
+            createProxyMiddleware({
+                target: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+                changeOrigin: true,
+                pathRewrite: (path, req) => '/v1/traces',
+            })
+        );
+    }
+
+    if (process.env.OTEL_METRICS_EXPORTER === 'otlp') {
+        console.info('Initialize OTEL metrics Proxy');
+        router.use(
+            '/telemetry/metrics',
+            createProxyMiddleware({
+                target: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+                changeOrigin: true,
+                pathRewrite: (path, req) => '/v1/metrics',
+            })
+        );
+    }
 }
 
 export default router;
